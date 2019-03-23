8c95e8ea46a6ea560d8a7d20384e845aee4ef999
DevOpsLab <48814936+devopslabinc@users.noreply.github.com>
2019-03-23 13:16:59 -0400

    Update variables.tf

--- aws-ec2-instance/variables.tf
+++ aws-ec2-instance/variables.tf
@@ -25,3 +25,10 @@ variable "user_data" {
   description = "Instance user data. Do not pass gzip-compressed data via this argument"
   default     = ""
 }
+variable "aws_key" {
+  description = "Name the key you will use to access the instance "
+  default     = "devopslab"
+
+
+
+
