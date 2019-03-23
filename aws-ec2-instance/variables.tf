51c58ac7668437652d161de795fddbda564c9950
DevOpsLab <48814936+devopslabinc@users.noreply.github.com>
2019-03-23 16:58:22 -0400

    Update variables.tf

--- aws-ec2-instance/variables.tf
+++ aws-ec2-instance/variables.tf
@@ -23,7 +23,7 @@ variable "associate_public_ip_address" {
 }
 variable "user_data" {
   description = "Instance user data. Do not pass gzip-compressed data via this argument"
-  default     = "")}"
+  default     = ""
 }
 variable "key_name" {
   description = "Name the key you will use to access the instance "
