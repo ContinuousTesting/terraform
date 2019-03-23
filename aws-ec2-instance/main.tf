d4568f38ca71766473902a9175df3fadfd302069
DevOpsLab <48814936+devopslabinc@users.noreply.github.com>
2019-03-22 20:02:47 -0400

    add  key_name

--- aws-ec2-instance/main.tf
+++ aws-ec2-instance/main.tf
@@ -10,8 +10,8 @@ resource "aws_instance" "awslinux" {
   ami           = "${var.ami_id}"
   instance_type = "${var.instance_type}"
   availability_zone = "${var.aws_region}a"
-   user_data = "${var.user_data}"
-
+  user_data = "${var.user_data}"
+  key_name = "devopslab"
   tags {
     Name = "${var.name}"
   }
