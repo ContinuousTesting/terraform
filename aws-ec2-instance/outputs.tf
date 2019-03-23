ec605580be14f9e0e94adaf7e2c986b02d2125ae
DevOpsLab <48814936+devopslabinc@users.noreply.github.com>
2019-03-22 20:30:58 -0400

    Update outputs.tf

--- aws-ec2-instance/outputs.tf
+++ aws-ec2-instance/outputs.tf
@@ -1,1 +1,8 @@
-
+output "public_dns" {
+  description = "List of public DNS names assigned to the instances"
+  value       = "${module.ec2.public_dns}"
+}
+output "instance_public_dns" {
+  description = "Public DNS name assigned to the EC2 instance"
+  value       = "${module.ec2.public_dns[0]}"
+}
