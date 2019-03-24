0dd088862bc72fdf60ba1f0f6dcc686d9e63225f
StanZv <stzvenig@gmail.com>
2019-03-23 20:18:48 -0400

    Rename add jenkins

---
+++ aws-ec2-instance-devtools/user_data/user_data.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# AWS linux ec2 instance Development env NodeJS, GIT, MAven, Java 1.8 awscli
+
+sudo yum update -y
+#install git
+sudo yum install -y git
+# Install apache maven
+sudo touch /etc/yum.repos.d/epel-apache-maven.repo
+sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+sudo yum install -y apache-maven
+# Install java-1.8.0
+sudo yum install -y java-1.8.0
+# sudo yum remove -y java-1.7.0-openjdk
+echo 2 | sudo update-alternatives --config java
+# Configure Java development env set Java home /ec2-user
+echo '
+export JAVA_HOME="/usr/lib/jvm/jre-1.8.0-openjdk.x86_64"
+PATH=$JAVA_HOME/bin:$PATH
+' >> /home/ec2-user/.bashrc
+# install AWS CLI
+sudo curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+sudo unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+# Install Nodejs
+sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+. ~/.nvm/nvm.sh
+export NVM_DIR="/home/ec2-user/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+nvm install 4.4.5
+node -e "console.log('Running Node.js ' + process.version)"
---
+++ jenkins-ec2-instance/README.md
@@ -0,0 +1,18 @@
+# Provision an EC2 instance With Jenkins , Git,Maven,NodeJs,Java and AWS cli in AWS Development enviroment
+
+Provisions a AWS Linux  Base Image AMI (with ID ami-0cd3dfa4e37921605) with type t2.micro in the uus-east-2a region. The AMI ID, region, and type can all be set as variables. You can also set the name variable to determine the value set for the Name tag.
+
+Note you need to set environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY.
+# Add corected values to >> $HOME/.bash_profile
+echo '
+export AWS_ACCESS_KEY_ID="****CHANGEME********"
+export AWS_SECRET_ACCESS_KEY="***CHANGEME*****"
+' >> $HOME/.bash_profile
+
+Usage :
+
+terraform init
+terraform plan
+terraform apply
+
+Jenkins will start on port 8080 
